66c40ed4f9a6af401e8e058df930a81982e077d3
iv4ann <iv4annp@gmail.com>
2025-08-12 22:25:35 -0600

    Update bd_las_menonas.sql

--- PF/bd_las_menonas.sql
+++ PF/bd_las_menonas.sql
@@ -6,7 +6,9 @@
 -- Generation Time: Aug 08, 2025 at 12:36 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
-
+CREATE DATABASE IF NOT EXISTS bd_las_menonas
+DEFAULT CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
